064f2c635837df51c9c43f05bf071d99d6de4251
abdohelgamal <34077810+abdohelgamal@users.noreply.github.com>
2022-01-08 01:15:02 +0200

    UI updates from database with every change

--- lib/Controller/database.dart
+++ lib/Controller/database.dart
@@ -3,6 +3,19 @@ import 'package:sqflite/sqflite.dart';
 class Dbcontroller {
   late Database database;
 
+    createDataBase() async {
+    String databasesPath = await getDatabasesPath();
+    databasesPath = databasesPath + '/mydatabase.db';
+    await openDatabase(databasesPath, version: 1).then((data) {
+      database = data;
+      print('database created');
+      database.transaction((txn) async {
+        return await txn.execute(
+            '''CREATE TABLE TestData (id integer primary key autoincrement, picture text, title text, description text, date text , status text)''');
+      });
+    });
+  }
+
   Future<void> insertIntoDataBase(String path, String title, String description,
       String date, String status) async {
     await database.insert('TestData', {
--- lib/Models/bloc.dart
+++ lib/Models/bloc.dart
@@ -2,36 +2,41 @@ import 'package:bloc/bloc.dart';
 import 'package:facegraph_assessment/Controller/database.dart';
 import 'package:facegraph_assessment/Models/issue_class.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:sqflite/sqflite.dart';
 
 class BlocForIssues extends Cubit<List<Issue>> {
   BlocForIssues() : super(List.empty()) {
     db = Dbcontroller();
-    createDataBase();
+    db.createDataBase().whenComplete((){
+        updateIssuesList();
+    });
+  
     print('bloc create');
   }
 
   late Dbcontroller db;
   List<Issue> issues = [];
+//write a function to check if there is a table in the database or not
 
-  createDataBase() async {
-    String databasesPath = await getDatabasesPath();
-    databasesPath = databasesPath + '/mydatabase.db';
-    await openDatabase(databasesPath, version: 1).then((database) {
-      db.database = database;
-      print('database created');
-      db.database.transaction((txn) async {
-        return await txn.execute(
-            '''CREATE TABLE TestData (id integer primary key autoincrement, picture text, title text, description text, date text , status text)''');
-      });
-    });
-  }
+  // createDataBase() async {
+  //   String databasesPath = await getDatabasesPath();
+  //   databasesPath = databasesPath + '/mydatabase.db';
+  //   await openDatabase(databasesPath, version: 1).then((database) {
+  //     db.database = database;
+  //     print('database created');
+  //     if (db.database == null) {
+  //       db.database.transaction((txn) async {
+  //         return await txn.execute(
+  //             '''CREATE TABLE TestData (id integer primary key autoincrement, picture text, title text, description text, date text , status text)''');
+  //       });
+  //     }
+  //   });
+  // }
 
   void updateIssuesList() async {
     List<Map> temp = await db.getdata();
     print(temp);
     issues = temp
-        .map((val) => Issue(val['id'], val['title'], XFile(val['picture']) ,
+        .map((val) => Issue(val['id'], val['title'], XFile(val['picture']),
             val['description'], val['date'], val['status']))
         .toList();
     emit(issues);
