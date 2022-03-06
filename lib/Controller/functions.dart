85e2752e1a700d24b40b89e69edef6590854c599
abdohelgamal <34077810+abdohelgamal@users.noreply.github.com>
2022-03-06 15:56:35 +0200

    dumb commit

--- lib/Controller/functions.dart
+++
@@ -1,9 +0,0 @@
-import 'package:image_picker/image_picker.dart';
-
-bool validate(String title, XFile? picture, String description) {
-  if (title != '' && picture != null && description != '') {
-    return true;
-  } else {
-    return false;
-  }
-}
